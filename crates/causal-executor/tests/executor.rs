49392561a04b7ca6c8548b48e7b07cd14c8bc420
Hans Moog <3293976+hmoog@users.noreply.github.com>
2025-09-16 00:54:21 +0200

    modifications

--- crates/causal-executor/tests/executor.rs
+++ crates/causal-executor/tests/executor.rs
@@ -1,51 +1,64 @@
+extern crate core;
+
+use std::thread::sleep;
+use std::time::Duration;
 use kas_l2_causal_executor::Executor;
 use kas_l2_causal_scheduler::{Scheduler, Task};
 
 #[test]
 pub fn test_executor() {
     let scheduler = Scheduler::<Transaction>::new();
-    let executor = Executor::new(4, |tx: &Transaction| {
-        println!("Executing task with id {}", tx.id);
-    });
-
-    executor.execute(scheduler.schedule(vec![
+    let batch1 = scheduler.schedule(vec![
         Transaction {
             id: 0,
+            duration: Duration::from_millis(10),
             write_locks: vec![1],
             read_locks: vec![3],
         },
         Transaction {
             id: 1,
+            duration: Duration::from_millis(1),
             write_locks: vec![1, 2],
             read_locks: vec![],
         },
         Transaction {
             id: 2,
+            duration: Duration::from_millis(1),
             write_locks: vec![],
             read_locks: vec![3],
         },
-    ]));
-
-    executor.execute(scheduler.schedule(vec![
+    ]);
+    let batch2 = scheduler.schedule(vec![
         Transaction {
-            id: 0,
+            id: 3,
+            duration: Duration::from_millis(1),
             write_locks: vec![1],
             read_locks: vec![3],
         },
         Transaction {
-            id: 1,
+            id: 4,
+            duration: Duration::from_millis(1),
             write_locks: vec![10, 20],
             read_locks: vec![],
         },
-    ]));
+    ]);
 
-    std::thread::sleep(std::time::Duration::from_secs(1));
+    let executor = Executor::new(4, |tx: &Transaction| {
+        println!("Executing transaction with id {}", tx.id);
+        sleep(tx.duration);
+        println!("Finished transaction with id {}", tx.id);
+    });
+    executor.execute(batch1);
+    executor.execute(batch2);
+
+    sleep(Duration::from_secs(1));
 
     executor.shutdown();
 }
 
 struct Transaction {
     id: u32,
+    duration: Duration,
     read_locks: Vec<u32>,
     write_locks: Vec<u32>,
 }
