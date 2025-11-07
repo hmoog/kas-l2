a3cf5e727b275d383306911afe0897214ed1fb0e
Hans Moog <3293976+hmoog@users.noreply.github.com>
2025-11-07 04:32:00 +0100

    add move-stdlib-natives to vm

--- move-runtime/vm/src/vm.rs
+++ move-runtime/vm/src/vm.rs
@@ -1,4 +1,5 @@
 use move_binary_format::errors::VMResult;
+use move_core_types::account_address::AccountAddress;
 use move_vm_runtime::move_vm::MoveVM;
 
 use crate::{
@@ -9,7 +10,14 @@ pub struct VM(MoveVM);
 
 impl VM {
     pub fn new() -> Self {
-        Self(MoveVM::new([]).expect("failed to initialize VM"))
+        Self(
+            MoveVM::new(move_stdlib_natives::all_natives(
+                AccountAddress::ONE,
+                move_stdlib_natives::GasParameters::zeros(),
+                false,
+            ))
+            .expect("failed to initialize VM"),
+        )
     }
 
     pub fn process(&self, tx: &Transaction, res: &mut [AccessHandle]) -> VMResult<()> {
